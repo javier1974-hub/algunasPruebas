a08f1ab42ada4602e81baaf7eec3dafe79df4737
javier <javier.m@veccsa.com>
2023-10-27 12:00:33 -0300

    parece que anda. el Layout bastante bien. falta visualizar los bpm

--- DeteccionValidacionRealTimePyQtGraphDeNuevo.py
+++ DeteccionValidacionRealTimePyQtGraphDeNuevo.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PyQt6.QtWidgets import (QApplication, QWidget, QLabel,
 QLineEdit, QCheckBox, QTextEdit, QGridLayout,QPushButton,QFileDialog, QTableWidget, QTableWidgetItem,QProgressBar)
+from PyQt6 import QtCore
 from pyqtgraph import PlotWidget, plot
 from PyQt6.QtCore import pyqtSignal, QThread
 import pyqtgraph as pg
@@ -13,9 +14,17 @@ import numpy as np
 import os
 import random
 import itertools
+import serial
 
 
+i = 0
+k = 100
+n = 5000
+cont = 0
+N = 0
 
+offset = 56500
+factor = 2000
 
 
 
@@ -46,23 +55,31 @@ class MainWindow(QWidget):
 
     def setUpMainWindow(self):
 
-        self.pcg=[]
-        self.preds = []
         self.win = pg.GraphicsLayoutWidget(show=True)
         self.label = pg.LabelItem(justify='right')
         self.win.addItem(self.label)
-        #self.r = pg.PolyLineROI([(0, 0), (10, 10)])
-
-
 
         self.p1 =self.win.addPlot(row=0, col=0)
         # customize the averaged curve that can be activated from the context menu:
         self.p1.avgPen = pg.mkPen('#FFFFFF')
         self.p1.avgShadowPen = pg.mkPen('#8080DD', width=10)
-        #self.p1.addItem(self.r)
 
+        self.x = np.zeros((n), dtype=np.float32)
+        self.y = np.zeros((n), dtype=np.float32)
+        self.R = np.zeros((n), dtype=np.float32)
 
+        self.x[:] = np.linspace((N * k), (n + (N * k)) - 1, n)
 
+        self.y_uart = np.zeros(k)
+        self.R_uart = np.zeros(k)
+
+        #self.p1.setBackground('k')
+        self.p1.showGrid(x=True, y=True, alpha=0.8)
+
+        pen = pg.mkPen(color='r', width=2)
+        self.data_line = self.p1.plot(self.x, self.y, pen=pen)
+        pen1 = pg.mkPen(color='g', width=3)
+        self.data_line1 = self.p1.plot(self.x, self.R, pen=pen1)
 
         self.bpm= QLabel("",self)
 
@@ -75,12 +92,17 @@ class MainWindow(QWidget):
 
         #self.table = QTableWidget(4,2)
 
-        self.items_grid = QGridLayout()
-        self.items_grid.addWidget(self.win , 0, 1, 5,1)
-        self.items_grid.addWidget(self.button_Connect, 0, 0, 1,1)
-        self.items_grid.addWidget(self.button_Disconnect, 2, 0, 1, 1)
-        self.items_grid.addWidget(self.bpm, 3, 0, 1, 1)
+        # self.items_grid = QGridLayout()
+        # self.items_grid.addWidget(self.win , 0, 1, 5,1)
+        # self.items_grid.addWidget(self.button_Connect, 0, 0, 1,1)
+        # self.items_grid.addWidget(self.button_Disconnect, 2, 0, 1, 1)
+        # self.items_grid.addWidget(self.bpm, 3, 0, 1, 1)
 
+        self.items_grid = QGridLayout()
+        self.items_grid.addWidget(self.win , 0, 0, 9, 2)
+        self.items_grid.addWidget(self.button_Connect, 10, 0, 1, 1)
+        self.items_grid.addWidget(self.button_Disconnect, 10, 1, 1, 1)
+        self.items_grid.addWidget(self.bpm, 3, 0, 1, 1)
 
 
         self.setLayout(self.items_grid)
@@ -88,9 +110,22 @@ class MainWindow(QWidget):
 
 
     def buttonConnectClicked(self, evt):
+        # initialize serial port
+        self.ser = serial.Serial()
+        self.ser.port = "COM3"  # Arduino serial port
+        self.ser.baudrate = 5000000
+        self.ser.timeout = None  # specify timeout when using readline()
+        self.ser.open()
+        if self.ser.is_open == True:
+            print("\nAll right, serial port now open. Configuration:\n")
+            print(self.ser, "\n")  # print serial parameters
 
-        pass
+        self.ser.flush()
 
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(10)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
 
     def buttonDisconnectClicked(self):
 
@@ -106,6 +141,38 @@ class MainWindow(QWidget):
         # self.worker.finished.connect(self.graficar)
 
         pass
+
+
+    def update_plot_data(self):
+        global k, i, N, cont
+
+        while i < k:
+            inicio_dato = self.ser.read(1)
+            if inicio_dato == b'\x24':
+                dato_uart_byte = self.ser.read(5)
+                fin_dato = self.ser.read(1)
+                if fin_dato == b'\x23':
+                    self.y_uart[i] = int.from_bytes(dato_uart_byte[0:4], byteorder='big', signed=True)
+                    self.R_uart[i] = int.from_bytes(dato_uart_byte[4:], byteorder='big', signed=True)
+                    i = i + 1
+                # else:
+                # fin_dato = ser.read(1)
+            else:
+                pass
+        else:
+            i = 0
+
+        self.x[:] = np.linspace((N * k), (n + (N * k)) - 1, n)
+        self.y[:-k] = self.y[k:]      # primero corro k a la izquierda
+        self.R[:-k] = self.R[k:]
+        self.y[-k:] = self.y_uart[:]  + offset # luego agrego los nuevos k valores a graficar
+        self.R[-k:] = self.R_uart[:] * factor
+
+        self.data_line.setData(self.x,self.y)  # Update the data.
+        self.data_line1.setData(self.x,(-1)*self.R+14000)  # Update the data.
+
+        N = N + 1
+
 
     def graficar(self,data):
         self.pcg = data
